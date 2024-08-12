5c1494484de7c579b8abe2fe144d3eda8a688246
Fern-Aerell <fernaerell2020@gmail.com>
2024-08-13 01:22:07 +0700

    Menghilangkan argument yang di console log pada perintah /args_test

--- src/commands/args_test.ts
+++ src/commands/args_test.ts
@@ -1,6 +1,5 @@
 import CommandHandler from "../core/CommandHandler";
 
 export async function execute(this: CommandHandler, args: string[]) {
-    console.log(args);
     this.wat.sendWatermarkTextToSelf(`Arguments:\n${args.join('\n')}`);
 }
